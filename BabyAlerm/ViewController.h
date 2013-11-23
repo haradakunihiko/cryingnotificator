eaa0630af9f1d84d261f32d4bbc3a43c1759e800
kunihiko-harada <sthsoulful@gmail.com>
2013-11-23 13:34:34 +0900

    Initial Commit

---
+++ BabyAlerm/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  BabyAlerm
+//
+//  Created by harada on 2013/11/23.
+//  Copyright (c) 2013年 harada. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ BabyAlerm/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BabyAlerm
+//
+//  Created by harada on 2013/11/23.
+//  Copyright (c) 2013年 harada. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
